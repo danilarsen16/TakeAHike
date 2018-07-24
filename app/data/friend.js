5df127375a5b4019d11abbdeb8cde7f72b2427ef
Dani Larsen <danilarsen16@yahoo.com>
2018-07-23 21:48:16 -0500

    first commit

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Dani Larsen...lucky!',
+    profilePic: 'https://avatars2.githubusercontent.com/u/38441292?s=460&v=4',
+    scores: [
+      "3",
+      "5",
+      "5",
+      "2",
+      "5",
+      "5",
+      "5",
+      "3",
+      "3",
+      "5"
+    ]
+  }
+]
+
+module.exports = friendList;
