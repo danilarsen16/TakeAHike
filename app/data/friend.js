11a6a9915d5ed9ee9a001861e75973d9f2d1e99d
Dani Larsen <danilarsen16@yahoo.com>
2018-09-11 21:47:27 -0500

    UI/UX

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,22 +1,166 @@
 //array that holds all the 'friends' that have taken the survey
 //initialize starting with 'dummy' data
 var friendList = [
-  {
+	{
     name: 'Dani Larsen...lucky!',
     profilePic: 'https://avatars2.githubusercontent.com/u/38441292?s=460&v=4',
-    scores: [
+		scores: [
       "3",
       "5",
       "5",
-      "2",
       "5",
       "5",
       "5",
-      "3",
-      "3",
+      "5",
+      "4",
+      "5",
       "5"
-    ]
-  }
-]
+		]
+	},
+	{
+		name: 'Lenny Pepperbottom',
+		photo: 'https://i.imgur.com/QfGP9qm.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Spencer',
+		photo: 'http://www.funnydogsite.com/pictures/Hiker_Dog.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'The dog whisperer',
+		photo: 'http://www.colinspack.com/wp-content/uploads/2014/05/IMG_5161-copy-940x430.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'This tired lady',
+		photo: 'https://i1.wp.com/thatsmycoffee.com/wp-content/uploads/2013/02/DSC_1093-small.jpg?fit=1546%2C1024',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Biggie Smalls',
+		photo: 'https://i.pinimg.com/736x/db/f2/56/dbf256c87fd10aecde1b8a778cb8a01c--cute-rats-a-rat.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'This bag of trail mix',
+		photo: 'https://www.njhiking.com/wp-content/uploads/2009/04/trail-mix.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Mr. "This spot looks good for camp"',
+		photo: 'http://en.bcdn.biz/images/emails_source/765f3e67-0bfd-4f20-83c1-3d57031a0546.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mrs. I should have brought other shoes',
+		photo: 'http://www.toxel.com/wp-content/uploads/2010/11/tevaheels07.jpg',
+		scores: [
+      '5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'This guy who should have picked July',
+		photo: 'http://kelseyguidebooks.com/wp-content/uploads/2012/01/2.-BlizzardMRKPackFarMasherbrum.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
 
 module.exports = friendList;
